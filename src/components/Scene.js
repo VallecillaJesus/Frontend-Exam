e264cf5972c99ca31bd92c4525a617f17d7d7872
VallecillaJesus <jesusvallecilla27@gmail.com>
2022-04-03 21:36:42 -0500

    Adding reset button to start again

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -6,7 +6,7 @@ export default class Scene extends Component {
   render() {
     return (
         <h1 className='historia'>
-          {this.props.scenary || 'FIN'}
+          {this.props.scenary || <button className='botones' onClick={() => window.location.reload()}>Volver a empezar</button>}
         </h1>
     )
   }
